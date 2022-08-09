4b4a2527e5575b703138ec2495c677458dd08cf8
amirhoseinsaneie <amirhoseinsaneie94@gmail.com>
2022-08-09 22:32:41 +0430

    Fix Some Bugs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from posixpath import dirname
 import subprocess
 import os
 import shutil
-src = './src'
+src = 'E:/2021-07-01 Matt/2021 REDACTED VIDEOS'
 dst = './mp4'
 
 for root, dirs, filenames in os.walk(src, topdown=False):
@@ -27,13 +27,13 @@ for root, dirs, filenames in os.walk(src, topdown=False):
             outputPath = root.replace(src , dst)
             if not os.path.exists(outputPath):
                 os.makedirs(outputPath);
-                outputfile = os.path.join(outputPath, filename.lower().replace(_format, ".mp4"))
+            outputfile = os.path.join(outputPath, filename.lower().replace(_format, ".mp4"))
                 # subprocess.call(['ffmpeg', '-i', inputfile, '-vf','scale=-1:720', '-c:v', 'h264_nvenc', '-crf', '25', '-preset' ,'veryslow' ,'-c:a' ,'copy' , outputfile])  
-                if _format != '' :
-                    subprocess.call(['ffmpeg','-hwaccel_device' ,'0' ,'-hwaccel' ,'cuda' ,'-i' , inputfile ,'-vf' ,'scale=-1:720' ,'-c:v' ,'h264_nvenc' ,'-preset' ,'slow' ,outputfile])
-                if not ".avi" in filename.lower():
-                    shutil.copyfile(os.path.join(root,filename),os.path.join(outputPath,filename))
-                    print(filename , 'Copied')
+            if _format != '' :
+                subprocess.call(['ffmpeg','-hwaccel_device' ,'0' ,'-hwaccel' ,'cuda' ,'-i' , inputfile ,'-vf' ,'scale=-1:720' ,'-c:v' ,'h264_nvenc' ,'-preset' ,'slow' ,outputfile])
+            if not ".avi" in filename.lower():
+                shutil.copyfile(os.path.join(root,filename),os.path.join(outputPath,filename))
+                print(filename , 'Copied')
             else:
                 break;
         except:     
