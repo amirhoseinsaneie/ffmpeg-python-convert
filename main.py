fa7c951290b14c9f7f9e8aedd234e75d98495401
amirhoseinsaneie <amirhoseinsaneie94@gmail.com>
2022-08-09 22:43:00 +0430

    Better File Exporting

--- main.py
+++ main.py
@@ -27,14 +27,13 @@ for root, dirs, filenames in os.walk(src, topdown=False):
             outputPath = root.replace(src , dst)
             if not os.path.exists(outputPath):
                 os.makedirs(outputPath);
+            
             outputfile = os.path.join(outputPath, filename.lower().replace(_format, ".mp4"))
-                # subprocess.call(['ffmpeg', '-i', inputfile, '-vf','scale=-1:720', '-c:v', 'h264_nvenc', '-crf', '25', '-preset' ,'veryslow' ,'-c:a' ,'copy' , outputfile])  
+               
             if _format != '' :
                 subprocess.call(['ffmpeg','-hwaccel_device' ,'0' ,'-hwaccel' ,'cuda' ,'-i' , inputfile ,'-vf' ,'scale=-1:720' ,'-c:v' ,'h264_nvenc' ,'-preset' ,'slow' ,outputfile])
-            if not ".avi" in filename.lower():
+            else:
                 shutil.copyfile(os.path.join(root,filename),os.path.join(outputPath,filename))
                 print(filename , 'Copied')
-            else:
-                break;
         except:     
             print("An exception occurred")
